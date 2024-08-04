a44a7a8771a920826f7a9c4366be1ec4ee5cf7f8
Nouf-901 <nllllr90@gmail.com>
2024-08-04 21:14:31 +0300

    hi2

---
+++ MyPlayground4.playground/Contents.swift
@@ -0,0 +1,46 @@
+import UIKit
+/*
+let day = "Tuse"
+
+switch day {
+case "Tuse":
+    print("To day is Tuseday")
+case "Mon":
+    print("To day is Monday")
+case "Sun":
+    print("To day is Sunday")
+default:
+    print("Another day")
+}
+
+let wether = "cold"
+
+switch wether {
+case "cold":
+    print("Plese wear a jacket")
+case "sunny":
+    print("Plese wear a Sunglasess")
+case "rainy":
+    print("Plese wear an umbrella")
+default:
+    print("Today a beatiful day ")
+}
+ */
+
+
+
+var mathMark = 90
+switch mathMark {
+case 90...100:
+    print("Exellent")
+case  80..<90:
+    print("Very good")
+case  70..<80:
+    print("Good")
+case  60..<70:
+    print("Pass")
+default:
+    print("Fail")
+}
+
+
